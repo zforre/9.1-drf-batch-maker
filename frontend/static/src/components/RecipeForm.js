f80ee80847df2dad85f73441b44156c4a01b2214
Zach Forrest <zachforre@gmail.com>
2020-03-05 14:53:06 -0500

    completes form component

--- frontend/static/src/components/RecipeForm.js
+++ frontend/static/src/components/RecipeForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-
 import axios from 'axios';
+import Form from 'react-bootstrap/Form'
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
@@ -11,21 +11,49 @@ class RecipeForm extends Component {
             this.state = {
                 recipes: [],
                 title: '',
-                description: ''
+                description: '',
+                image: null,
+                preview: null,
+                is_public: true,
+                recipe_type: "",
+                prep_time: "",
+                cook_time: "",
+                cook_temp: 1,
+                directions: "",
             }
 
-            this.handleInput = this.handleInput.bind(this);
+            this.handleChange = this.handleChange.bind(this);
             this.handleSubmit = this.handleSubmit.bind(this);
+            this.handleImageChange = this.handleImageChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        this.setState({[name]: value})
+    }
+
+    handleImageChange(event){
+        let file = event.target.files[0]
+        this.setState({image: file});
+        let reader = new FileReader();
+        reader.onloadend = () => {
+        this.setState({preview: reader.result});
+        };
+
+        reader.readAsDataURL(file);
+
     }
 
     componentDidMount() {
-        axios.get(`api/v1/`)
-          .then(response => this.setState({recipes: response.data}))
-          .catch(error => console.log(error));
-    }
-
-    handleInput(event) {
-        this.setState({[event.target.name]: event.target.value});
+        axios.get("/api/v1/")
+        .then(result => {
+            this.setState({recipes: result.data})
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     handleSubmit(event) {
@@ -34,17 +62,80 @@ class RecipeForm extends Component {
         formData.append("title", this.state.title);
         formData.append("image", this.state.image);
 
+        axios.post("/api/v1/recipes/", formData, {
+            headers: {
+                "content-type": "multipart/form-data"
+            }
+        })
+        .then(res => {
+            let recipes = [...this.state.recipes];
+            recipes.push(res.data);
+
+            this.setState({recipes: recipes, title: "", preview: null, image: null,});
+        })
+        .catch(error => {
+            console.log(error)
+        });
+
     }
 
     render() {
+        let recipes = this.state.recipes.map(recipe => (
+            <li key={recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt=""/>
+            </li>
+        ))
         return(
-            <form onSubmit={this.handleSubmit} className='row no-gutters'>
-                <div className='form-group col-12'>
-                    <label htmlFor='text'>What's the recipe's name?</label>
-                    <input className="form-control" type="text" name="text" id="text" value={this.state.title} onChange={this.handleInput} />
-                </div>
-                <button className='btn btn-primary'>Add</button>
-            </form>
+            <React.Fragment>
+                <form className="" onSubmit={this.handleSubmit}>
+                    <div className="form-group col-3">
+                        <label> Title</label>
+                            <input type="text" name="title" className="form-control" value={this.state.title} onChange={this.handleChange}/>
+                            <small className="form-text text-muted">This is the name of your recipe, make it special!</small>
+                    </div>
+
+                    <label> Image:
+                        <input type="file" name="image" onChange={this.handleImageChange}/>
+                    </label>
+
+                   <Form.Check type="checkbox" name="is_public" class="form-check-label" checked={this.state.is_public} onChange={this.handleChange} label="public" />
+
+                   <div className="form-group col-6">
+                        <label>Prep Time
+                            <input type="text" name="prep_time" className="form-control" value={this.state.prep_time} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+                    <div className="form-group col-6">
+                        <label>Cook Time
+                            <input type="text" name="cook_time" className="form-control" value={this.state.cook_time} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+                    <div className="form-group col-6">
+                        <label>Cook Temp
+                            <input type="number" name="cook_temp" className="form-control" value={this.state.cook_temp} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+                    <div className="form-group col-6">
+                        <label>Directions
+                            <input type="text" name="directions" className="form-control" value={this.state.directions} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+
+                    {this.state.image ? (
+                        <img src={this.state.preview} alt="preview"/>
+                    ) : (
+                        null
+                    )}
+
+                    <button className="btn btn-primary">Add Recipe</button>
+                </form>
+                <ul>{recipes}</ul>
+            </React.Fragment>
         )
     }
 }
